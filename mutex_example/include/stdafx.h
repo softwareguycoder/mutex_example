5cf6e49986eba1f97cbcfb545f06cbef3ce908d0
softwareguycoder <softwareguycoder@gmail.com>
2019-01-04 09:13:01 -0500

    4 Jan 2019 Added include guards

--- mutex_example/include/stdafx.h
+++ mutex_example/include/stdafx.h
@@ -2,12 +2,17 @@
 // for widely-used system includ files and headers
 //
 
+#ifndef __STDAFX_H__
+#define __STDAFX_H__
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
 // Exit codes
-#define OK		0					// Code to return to the operating system to indicate successful program termination
-#define ERROR	-1					// Code to return to the operating system to indicate an error condition
+#define OK		0		// Code to return to the operating system to indicate successful program termination
+#define ERROR	-1		// Code to return to the operating system to indicate an error condition
 
 typedef enum { FALSE, TRUE } BOOL;
+
+#endif //__STDAFX_H__
