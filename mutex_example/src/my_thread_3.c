8b2ec353b6596b13d162c94fe7760567ec1c2192
softwareguycoder <softwareguycoder@gmail.com>
2019-01-09 08:59:54 -0500

    9 Jan 2019 Added function name to logging

--- mutex_example/src/my_thread_3.c
+++ mutex_example/src/my_thread_3.c
@@ -18,6 +18,8 @@ void *my_thread_function_3(void *vargp)
 {
 	log_info("In my_thread_function_3");
 
+	log_info("my_thread_function_3: Checking whether there are any args...");
+
 	/* Check to ensure that the argument, vargp, is not a NULL pointer.
 	 * If it is, then warn the user but do not stop the function. */
 	if (vargp == NULL) {
@@ -30,28 +32,32 @@ void *my_thread_function_3(void *vargp)
 	// NOTE: 'lock' is a global variable we've defined in mutex_example.h
 	// and implemented in mutex_example.c
 
-	log_info("Attempting to get a mutually-exclusive lock...");
+	log_info("my_thread_function_3: Attempting to get a mutually-exclusive lock on the global mutex...");
 
 	LockMutex(hGlobalMutex);
 	{
-		log_info("Obtained mutually-exclusive lock.");
+		log_info("my_thread_function_3: Obtained mutually-exclusive lock.");
 
-		log_info("Counter before job start = %d", counter);
+		log_info("my_thread_function_3: Counter before job start = %d", counter);
 
 		counter+=1;
 
+		log_info("my_thread_function_3: Value of counter after increment = %d", counter);
+
 		log_info("Beginning job #%d...", counter);
+
+		log_info("my_thread_function_3: Doing work...");
 
 		/* This for loop is just here to make this thread last a long time */
 		for(unsigned long i = 0; i < LONG_MAX; i++);
 
-		log_info("Job #%d is done.", counter);
+		log_info("my_thread_function_3: Job #%d is done.", counter);
 
-		log_info("Releasing the mutually-exclusive lock...");
+		log_info("my_thread_function_3: Releasing the mutually-exclusive lock...");
 	}
 	UnlockMutex(hGlobalMutex);
 
-	log_info("Mutually-exclusive lock released.");
+	log_info("my_thread_function_3: Mutually-exclusive lock released.");
 
 	log_info(MY_THREAD_FUNCTION_3_DONE);
 
