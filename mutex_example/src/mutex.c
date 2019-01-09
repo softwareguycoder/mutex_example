c2d2d69cc92d00a3ebd5507cab7cce6060d68220
softwareguycoder <softwareguycoder@gmail.com>
2019-01-09 08:04:29 -0500

    9 Jan 2019 Added FreeMutex function to de-allocate mutex handles from memory

--- mutex_example/src/mutex.c
+++ mutex_example/src/mutex.c
@@ -3,27 +3,69 @@
 #include "stdafx.h"
 #include "mutex.h"
 
-
 /**
  * @brief Creates a mutex object, and returns a handle to it.  Returns INVALID_HANDLE_VALUE
-   if an error occurred.
+ if an error occurred.
  */
-HMUTEX CreateMutex()
-{
-	pthread_mutex_t* pMutex = (pthread_mutex_t*)malloc(sizeof(pthread_mutex_t));
-	if (pMutex == NULL)
-	{	
+HMUTEX CreateMutex() {
+	pthread_mutex_t* pMutex = (pthread_mutex_t*) malloc(
+			sizeof(pthread_mutex_t));
+	if (pMutex == NULL) {
 		return INVALID_HANDLE_VALUE;
 	}
 
 	// Call pthread_mutex_init.  This version of CreateMutex just passes a
 	// mutex handle for the function to initialize with NULL for the attributes.
-	if (OK != pthread_mutex_init(pMutex, NULL))
-	{
+	if (OK != pthread_mutex_init(pMutex, NULL)) {
+		// Cleanup the mutex handle if necessary
+		if (pMutex != NULL) {
+
+		}
+
 		return INVALID_HANDLE_VALUE;
 	}
 
-	return (HMUTEX)pMutex;	// a HMUTEX is a typedef of pthread_mutex_t*
+	return (HMUTEX) pMutex;	// a HMUTEX is a typedef of pthread_mutex_t*
+}
+
+/**
+ * @brief Releases resources associated with the specified mutex back to the operating system.
+ */
+void DestroyMutex(HMUTEX hMutex) {
+	if (INVALID_HANDLE_VALUE == hMutex) {
+		// If we have an invalid handle (i.e., NULL pointer), then there is nothing to do.
+		return;
+	}
+
+	// The HMUTEX handle type is just a typedef of pthread_mutex_t*
+	// However, to work with the pthread functions, we need to view it
+	// as such.
+	pthread_mutex_t* pMutex = (pthread_mutex_t*) hMutex;
+
+	pthread_mutex_destroy((pthread_mutex_t*) hMutex);
+
+	FreeMutex(hMutex);
+}
+
+/**
+ * @brief Internal (i.e., will not be put in the mutex.h header file) method
+ * for freeing malloc'd mutex handles.
+ * @param hMutex The handle to be freed.
+ */
+void FreeMutex(HMUTEX hMutex) {
+	if (INVALID_HANDLE_VALUE == hMutex) {
+		// If we have an invalid handle (i.e., NULL pointer), then there is nothing to do.
+		return;
+	}
+
+	// The HMUTEX handle type is just a typedef of pthread_mutex_t*
+	// However, to work with the pthread functions, we need to view it
+	// as such.
+	pthread_mutex_t* pMutex = (pthread_mutex_t*) hMutex;
+
+	free(pMutex);
+	pMutex = NULL;
+	hMutex = INVALID_HANDLE_VALUE;
 }
 
 /**
@@ -32,10 +74,8 @@ HMUTEX CreateMutex()
  * @param hMutex Handle of type HMUTEX (see stdafx.h) that refers to the mutex you want
  * to obtain a lock for.
  */
-void LockMutex(HMUTEX hMutex)
-{
-	if (hMutex == INVALID_HANDLE_VALUE)
-	{
+void LockMutex(HMUTEX hMutex) {
+	if (hMutex == INVALID_HANDLE_VALUE) {
 		return;
 	}
 
@@ -48,35 +88,11 @@ void LockMutex(HMUTEX hMutex)
  * @param hMutex Mutex handle of type HMUTEX (see stdafx.h) that refers to the mutex you want
  * to release the lock on.
  */
-void UnlockMutex(HMUTEX hMutex)
-{
-	if (hMutex == INVALID_HANDLE_VALUE)
-	{
+void UnlockMutex(HMUTEX hMutex) {
+	if (hMutex == INVALID_HANDLE_VALUE) {
 		return;
 	}
 
 	pthread_mutex_unlock(hMutex);
 }
 
-/**
- * @brief Releases resources associated with the specified mutex back to the operating system.
- */
-void DestroyMutex(HMUTEX hMutex)
-{
-	if (INVALID_HANDLE_VALUE == hMutex)
-	{	
-		// If we have an invalid handle (i.e., NULL pointer), then there is nothing to do.
-		return;
-	}
-
-	// The HMUTEX handle type is just a typedef of pthread_mutex_t*
-	// However, to work with the pthread functions, we need to view it
-	// as such.
-	pthread_mutex_t* pMutex = (pthread_mutex_t*)hMutex;
-
-	pthread_mutex_destroy((pthread_mutex_t*)hMutex);
-
-	free(pMutex);
-	pMutex = NULL;
-	hMutex = INVALID_HANDLE_VALUE;
-}
